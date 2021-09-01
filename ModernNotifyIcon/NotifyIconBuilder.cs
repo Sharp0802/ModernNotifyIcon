3d9a3ced043b10cbb23704b5a24116e2ef667730
Sharp0802 <nrs070802@gmail.com>
2021-09-01 21:11:31 +0900

    Add notify icon builder

---
+++ ModernNotifyIcon/NotifyIconBuilder.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace ModernNotifyIcon
+{
+	public class NotifyIconBuilder
+	{
+		private ContextMenuStripBuilder StripBuilder { get; } = new();
+
+		protected NotifyIconBuilder()
+		{
+		}
+
+		public static NotifyIconBuilder Create()
+		{
+			return new NotifyIconBuilder();
+		}
+
+		public NotifyIconBuilder Configure(Action<ContextMenuStripBuilder> builder)
+		{
+			builder(StripBuilder);
+			return this;
+		}
+
+		public NotifyIcon Build(Icon icon)
+		{
+			return new NotifyIcon
+			{
+				Icon = icon,
+				ContextMenuStrip = StripBuilder.Build()
+			};
+		}
+	}
+}
