5f08f4b35f6c7064f490cfcf19cad112af6099f8
Sharp0802 <nrs070802@gmail.com>
2021-09-01 12:55:00 +0900

    Add menu strip builder

---
+++ ModernNotifyIcon/ContextMenuStripBuilder.cs
@@ -0,0 +1,25 @@
+﻿using ModernNotifyIcon.Theme;
+
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace ModernNotifyIcon
+{
+    public class ContextMenuStripBuilder
+    {
+        protected List<ToolStripMenuItem> Items { get; } = new(); 
+
+        public ContextMenuStripBuilder AddItem(ToolStripMenuItem item)
+        {
+            Items.Add(item);
+            return this;
+        }
+
+        public ThemeReferencedContextMenuStrip Build()
+        {
+            var strip = new ThemeReferencedContextMenuStrip();
+            strip.Items.AddRange(Items.ToArray());
+            return strip;
+        }
+    }
+}
