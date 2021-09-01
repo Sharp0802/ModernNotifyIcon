9954836424b343aa952a37c73f3f89e6515391dd
Sharp0802 <nrs070802@gmail.com>
2021-09-01 13:44:24 +0900

    Merge source for ToggleGenerateOption

--- ModernNotifyIcon/ContextMenuStripBuilder.cs
+++ ModernNotifyIcon/ContextMenuStripBuilder.cs
@@ -20,32 +20,6 @@ namespace ModernNotifyIcon
 
         public ContextMenuStripBuilder AddSeperator() => AddItem(new ToolStripSeparator());
 
-        public sealed class ToggleGenerateOption
-        {
-            public delegate void ToggleEventHandler(bool toggled);
-
-            public string? Text { get; private set; }
-
-            public event ToggleEventHandler? Toggled;
-
-            public ToggleGenerateOption SetText(string text)
-            {
-                Text = text;
-                return this;
-            }
-
-            public ToggleGenerateOption AddHandler(ToggleEventHandler handler)
-            {
-                Toggled += handler;
-                return this;
-            }
-
-            internal void InvokeHandlers(bool check)
-            {
-                Toggled?.Invoke(check);
-            }
-        }
-
         public ContextMenuStripBuilder AddToggle(Action<ToggleGenerateOption> option)
         {
             var optionRef = new ToggleGenerateOption();
---
+++ ModernNotifyIcon/ToggleGenerateOption.cs
@@ -0,0 +1,28 @@
+﻿namespace ModernNotifyIcon
+{
+    public sealed class ToggleGenerateOption
+    {
+        public delegate void ToggleEventHandler(bool toggled);
+
+        public string? Text { get; private set; }
+
+        public event ToggleEventHandler? Toggled;
+
+        public ToggleGenerateOption SetText(string text)
+        {
+            Text = text;
+            return this;
+        }
+
+        public ToggleGenerateOption AddHandler(ToggleEventHandler handler)
+        {
+            Toggled += handler;
+            return this;
+        }
+
+        internal void InvokeHandlers(bool check)
+        {
+            Toggled?.Invoke(check);
+        }
+    }
+}
