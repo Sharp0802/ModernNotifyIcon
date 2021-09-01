6e4846bb782e3b9ad69aba750c647cf7c8a664be
Sharp0802 <nrs070802@gmail.com>
2021-09-01 21:09:12 +0900

    Refactoring code

--- ModernNotifyIcon/Theme/ThemeReferencedRenderer.cs
+++ ModernNotifyIcon/Theme/ThemeReferencedRenderer.cs
@@ -11,7 +11,9 @@ namespace ModernNotifyIcon.Theme
 	{
 		public int VerticalPadding { get; set; }
 
-		public ThemeReferencedRenderer() : base(new ThemeReferencedColorTable()) { }
+		public ThemeReferencedRenderer() : base(new ThemeReferencedColorTable())
+		{
+		}
 
 		protected override void OnRenderArrow(ToolStripArrowRenderEventArgs args)
 		{
@@ -19,10 +21,11 @@ namespace ModernNotifyIcon.Theme
 			var r = new Rectangle(args.ArrowRectangle.Location, args.ArrowRectangle.Size);
 			r.Inflate(-2, -4);
 			using var pen = new Pen(ThemeDictionary.TextFillColorPrimary);
-			args.Graphics.DrawLines(pen, new Point[] {
-				new Point(r.Left, r.Top),
-				new Point(r.Right, r.Top + r.Height / 2),
-				new Point(r.Left, r.Top+ r.Height)
+			args.Graphics.DrawLines(pen, new Point[]
+			{
+				new(r.Left, r.Top),
+				new(r.Right, r.Top + r.Height / 2),
+				new(r.Left, r.Top + r.Height)
 			});
 		}
 
@@ -33,26 +36,27 @@ namespace ModernNotifyIcon.Theme
 			var r = new Rectangle(point, args.ImageRectangle.Size);
 			r.Inflate(-2, -4);
 			using var pen = new Pen(ThemeDictionary.TextFillColorPrimary);
-			args.Graphics.DrawLines(pen, new Point[] {
-				new Point(r.Left, r.Bottom - r.Height / 2),
-				new Point((int)(r.Left + r.Width / 2.5f),  r.Bottom),
-				new Point(r.Right, r.Top)
+			args.Graphics.DrawLines(pen, new Point[]
+			{
+				new(r.Left, r.Bottom - r.Height / 2),
+				new((int) (r.Left + r.Width / 2.5f), r.Bottom),
+				new(r.Right, r.Top)
 			});
 		}
 
-        protected override void OnRenderSeparator(ToolStripSeparatorRenderEventArgs args)
-        {
+		protected override void OnRenderSeparator(ToolStripSeparatorRenderEventArgs args)
+		{
 			args.Graphics.SmoothingMode = SmoothingMode.AntiAlias;
 			var r = args.Item.ContentRectangle;
 			using var pen = new Pen(ThemeDictionary.BaseMedium);
 			args.Graphics.DrawLines(pen, new Point[]
 			{
-				new Point(r.Left + 5, (r.Bottom + r.Top) / 2),
-				new Point(r.Right - 5, (r.Bottom + r.Top) / 2)
+				new(r.Left + 5, (r.Bottom + r.Top) / 2),
+				new(r.Right - 5, (r.Bottom + r.Top) / 2)
 			});
 		}
 
-        protected override void OnRenderItemText(ToolStripItemTextRenderEventArgs args)
+		protected override void OnRenderItemText(ToolStripItemTextRenderEventArgs args)
 		{
 			args.TextFormat &= ~TextFormatFlags.HidePrefix;
 			args.TextFormat |= TextFormatFlags.VerticalCenter;
@@ -62,14 +66,14 @@ namespace ModernNotifyIcon.Theme
 			base.OnRenderItemText(args);
 		}
 
-        protected override void OnRenderMenuItemBackground(ToolStripItemRenderEventArgs args)
+		protected override void OnRenderMenuItemBackground(ToolStripItemRenderEventArgs args)
 		{
 			if (args.Item.Selected)
-            {
+			{
 				args.Graphics.SmoothingMode = SmoothingMode.AntiAlias;
 				using var brush = new SolidBrush(ThemeDictionary.ListMedium);
 				args.Graphics.FillRectangle(brush, new Rectangle(2, 0, args.Item.Width - 4, args.Item.Height - 1));
-            }
+			}
 		}
-    }
+	}
 }
