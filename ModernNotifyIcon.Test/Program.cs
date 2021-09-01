fce71445ecab8540c40159943cc86dbb7a1c893c
Sharp0802 <nrs070802@gmail.com>
2021-09-01 21:49:50 +0900

    Add test project

---
+++ ModernNotifyIcon.Test/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Drawing;
+using System.Reflection;
+using System.Windows.Forms;
+using ModernNotifyIcon;
+
+var notification = NotifyIconBuilder
+	.Create()
+	.Configure(builder => builder
+		.AddText("Sample text")
+		.AddSeparator()
+		.AddButton(option => option
+			.SetText("Sample button")
+			.AddHandler(() => { /* On state changed */ }))
+		.AddToggle(option => option
+			.SetText("Sample toggle")
+			.AddHandler(toggled => { /* One handler */ })
+			.AddHandler(toggled => { /* Another handler */ })))
+	.Build(Icon.ExtractAssociatedIcon(Assembly.GetExecutingAssembly().Location)!);
+
+notification.Visible = true;
+
+Application.Run();
