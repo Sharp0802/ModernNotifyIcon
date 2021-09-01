92119e96fc41325197e7ed1f046822303b8b65c4
Sharp0802 <nrs070802@gmail.com>
2021-09-01 13:45:28 +0900

    Merge source for ButtonGenerateOption

---
+++ ModernNotifyIcon/ButtonGenerateOption.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ModernNotifyIcon
+{
+    public sealed class ButtonGenerateOption
+    {
+        public string? Text { get; private set; }
+
+        public event Action? Toggled;
+
+        public ButtonGenerateOption SetText(string text)
+        {
+            Text = text;
+            return this;
+        }
+
+        public ButtonGenerateOption AddHandler(Action handler)
+        {
+            Toggled += handler;
+            return this;
+        }
+
+        internal void InvokeHandlers()
+        {
+            Toggled?.Invoke();
+        }
+    }
+}
--- ModernNotifyIcon/ContextMenuStripBuilder.cs
+++ ModernNotifyIcon/ContextMenuStripBuilder.cs
@@ -29,30 +29,6 @@ namespace ModernNotifyIcon
             return this;
         }
 
-        public sealed class ButtonGenerateOption
-        {
-            public string? Text { get; private set; }
-
-            public event Action? Toggled;
-
-            public ButtonGenerateOption SetText(string text)
-            {
-                Text = text;
-                return this;
-            }
-
-            public ButtonGenerateOption AddHandler(Action handler)
-            {
-                Toggled += handler;
-                return this;
-            }
-
-            internal void InvokeHandlers()
-            {
-                Toggled?.Invoke();
-            }
-        }
-
         public ContextMenuStripBuilder AddButton(Action<ButtonGenerateOption> option)
         {
             var optionRef = new ButtonGenerateOption();
